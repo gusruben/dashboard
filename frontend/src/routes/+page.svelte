a25ae270bb573b075f59fb2df022f4a7304683cd
Gus Ruben <gusruben@protonmail.com>
2025-07-30 22:28:53 -0400

    Initial bento grid

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="min-h-screen bg-gray-950 p-8 flex items-center justify-center">
+	<div class="grid grid-cols-10 grid-rows-6 gap-4 w-full max-w-4xl aspect-[5/3]">
+		<!-- clock/weather, github/hackatime graph, weighted grants, and airtable stats -->
+        <div class="bg-primary-500 rounded-3xl col-span-4 row-span-5"></div>
+
+        <!-- quick links -->
+        <div class="bg-gray-900 rounded-3xl col-span-1 row-span-1"></div>
+		<div class="bg-gray-900 rounded-3xl col-span-1 row-span-1"></div>
+		<div class="bg-gray-900 rounded-3xl col-span-1 row-span-1"></div>
+		
+        <!-- bus info OR misc widgets -->
+        <div class="bg-gray-900 rounded-3xl col-span-3 row-span-6"></div>
+		
+        <!-- hackatime stats + ai prompt -->
+        <div class="bg-gray-900 rounded-3xl col-span-3 row-span-2"></div>
+        <div class="bg-gray-900 rounded-3xl col-span-3 row-span-2"></div>
+		
+        <!-- web search -->
+		<div class="bg-gray-900 rounded-3xl col-span-7 row-span-1"></div>
+	</div>
+</div>
